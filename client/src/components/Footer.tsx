309ad12b133d1f5b97697150506687ddcf7518dc
Alex Taranne <alextaranne@gmail.com>
2025-02-06 22:14:15 +0100

    footer: done

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -1,7 +1,24 @@
+import "../styles/footer.css";
+import logo from "/cranepirate.png";
+
 export default function Footer() {
   return (
-    <>
-      <h2>Hello from Footer</h2>
-    </>
+    <footer>
+      <h2 className="footer-title">
+        Cette embarcation appartient à notre équipage!
+      </h2>
+      <div className="mid-footer">
+        <img src={logo} alt="" className="imgfooter" />
+        <ul className="team">
+          <li>L’Ombre du Vieux Kraken (Christophe)</li>
+          <li>La Sirène Enragée (Clarelle)</li>
+          <li>Le Marins des Profondeurs (Maxime)</li>
+          <li>Le Vieux Pirate au Rhum (Alex)</li>
+        </ul>
+      </div>
+      <p className="footer-title">
+        © 2025 Pirates Crew - Tous droits réservés. Argh!
+      </p>
+    </footer>
   );
 }
