1814ac9799fa11b665e9c67d674ce556335def25
Christophe Barth <christophe.moana.barth@gmail.com>
2025-02-07 11:20:06 +0100

    Weather just need a little touch of css

--- client/src/pages/Weather.tsx
+++ client/src/pages/Weather.tsx
@@ -1,3 +1,28 @@
-export default function Weather() {
-  return <h1>Weather</h1>;
-}
+import { useEffect } from "react";
+import "../styles/Weather.css";
+
+const Weather = () => {
+  useEffect(() => {
+    if (
+      !document.querySelector(
+        'script[src="https://static.elfsight.com/platform/platform.js"]',
+      )
+    ) {
+      const script = document.createElement("script");
+      script.src = "https://static.elfsight.com/platform/platform.js";
+      script.async = true;
+      document.body.appendChild(script);
+    }
+  }, []);
+
+  return (
+    <div className="weather-container">
+      <div
+        className="elfsight-app-a7a09335-1dfb-4d76-ae45-d79db2151f34"
+        data-elfsight-app-lazy
+      />
+    </div>
+  );
+};
+
+export default Weather;
